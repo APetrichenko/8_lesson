c5daa039dc37311acf06f492c0b32b9a3772e440
Valentina <raton13@rambler.ru>
2015-07-14 21:42:59 +0300

    iterator

---
+++ src/oop_animals/AnimalUtils.java
@@ -0,0 +1,10 @@
+package oop_animals;
+
+/**
+ * Created by ITHILLEL6 on 14.07.2015.
+ */
+public class AnimalUtils {
+    public static void standardRoar() {
+        System.out.println("ROARRRR!!");
+    }
+}
---
+++ src/oop_animals/Roarable.java
@@ -0,0 +1,11 @@
+package oop_animals;
+
+/**
+ * Created by ITHILLEL6 on 14.07.2015.
+ */
+interface Roarable {
+    default void roar(){
+        ///
+    }
+}
+
